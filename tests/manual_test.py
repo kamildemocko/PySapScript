a7e4c3ffd983bfcc0c54fd2bae26e9a58fd8d8bb
Kamil Democko <kamil.democko@gmail.com>
2023-12-17 17:48:52 +0100

    updated test, to test restore and visualize

--- tests/manual_test.py
+++ tests/manual_test.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 
 from pysapscript.pysapscript import Sapscript
 from pysapscript.types_.types import NavigateAction
@@ -15,15 +16,23 @@ class TestRuns:
             sid="SQ8",
             client="006",
             user="robot01_t",
-            password=str(pwd)
+            password=str(pwd),
+            quit_auto=False
         )
 
         self.window = self.pss.attach_window(0, 0)
 
     def test_runs(self):
+        # Maximize and Restore
+        self.window.restore()
+        sleep(1)
+        self.window.maximize()
+        sleep(1)
+
         # Basic actions
         self.window.start_transaction("se16")
         self.window.write("wnd[0]/usr/ctxtDATABROWSE-TABLENAME", "LFA1")
+        self.window.visualize("wnd[0]/usr/ctxtDATABROWSE-TABLENAME", 2)
         self.window.navigate(NavigateAction.enter)
 
         self.window.press("wnd[0]/tbar[1]/btn[31]")
@@ -43,9 +52,13 @@ class TestRuns:
 
         # New window
         self.pss.open_new_window(window_to_handle_opening=self.window)
+        sleep(2)  # ! This will not await the window as other window is opened with same title
         window2 = self.pss.attach_window(0, 1)
         window2.start_transaction("SQVI")
         window2.close_window()
+
+        # Quitting
+        self.pss.quit()
 
 
 if __name__ == "__main__":
